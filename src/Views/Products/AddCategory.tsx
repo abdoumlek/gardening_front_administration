1f39cd03ecf4ad7d46c208a72b00374c088ba79c
Youssef ABDELMALEK <abdelmalekyoussef@hotmail.com>
2020-07-11 18:37:05 +0200

    finished adding product

--- src/Views/Products/AddCategory.tsx
+++ src/Views/Products/AddCategory.tsx
@@ -1,12 +1,27 @@
-import React, { FC } from "react";
-import { IKContext, IKUpload } from "imagekitio-react";
+import React, { FC, useState } from "react";
+import axios from "axios";
 
 const AddProduct: FC<any> = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const postCategory = (name, description) => {
+    axios
+      .post("https://plantes-et-jardins-back.herokuapp.com/api/categories", {
+        name: name,
+        description: description,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((e) => console.error(e));
+  };
   return (
     <div>
       <h1>Ajouter une categorie de produits</h1>
       <div className="input-group mb-3">
         <input
+          value={name}
+          onChange={(htmlElement) => setName(htmlElement.target.value)}
           placeholder="Nom de la category"
           type="text"
           className="form-control"
@@ -16,12 +31,20 @@ const AddProduct: FC<any> = () => {
       </div>
       <div className="input-group mb-3">
         <textarea
+          value={description}
+          onChange={(htmlElement) => setDescription(htmlElement.target.value)}
           placeholder="Description de la category"
           className="form-control"
           aria-label="With textarea"
         ></textarea>
       </div>
-      <button type="button" className="btn btn-success">Ajouter le produit</button>
+      <button
+        type="button"
+        onClick={() => postCategory(name, description)}
+        className="btn btn-success"
+      >
+        Ajouter la category
+      </button>
     </div>
   );
 };
--- src/Views/Products/AddProduct.tsx
+++ src/Views/Products/AddProduct.tsx
@@ -1,12 +1,65 @@
-import React, { FC } from "react";
+import React, { FC, useState, useEffect } from "react";
 import { IKContext, IKUpload } from "imagekitio-react";
+import axios from "axios";
 
 const AddProduct: FC<any> = () => {
+  const [categories, setCategories] = useState<any>([]);
+  useEffect(() => {
+    const getCategories = () => {
+      axios
+        .get("https://plantes-et-jardins-back.herokuapp.com/api/categories")
+        .then((response) => {
+          setCategories(response.data);
+        })
+        .catch((e) => console.error(e));
+    };
+    getCategories();
+  }, []);
+
+  const [name, setName] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [category, setCategory] = useState<number>(1);
+  const [imageUrl, setImageUrl] = useState<string>("/shekhar-chandra-sahu-72P_hMW2l_g-unsplash_6Md5qI8a-.jpg");
+  const [sellingPrice, setSellingPrice] = useState<number>(0);
+  const [buyingPrice, setBuyingPrice] = useState<number>(0);
+  const [quantity, setQuantity] = useState<number>(0);
+  const [discount, setDiscount] = useState<number>(0);
+
+  const postProduct = (
+    name,
+    description,
+    category,
+    imageUrl,
+    sellingPrice,
+    buyingPrice,
+    quantity,
+    discount
+  ) => {
+    axios
+      .post("https://plantes-et-jardins-back.herokuapp.com/api/products", {
+        name: name,
+        description: description,
+        category_id: category,
+        photo: imageUrl,
+        selling_price: sellingPrice,
+        buying_price: buyingPrice,
+        quantity: quantity,
+        discount: discount,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((e) => console.error(e));
+  };
+
   return (
     <div>
       <h1>Ajouter un produit</h1>
+      <label>Nom du produit</label>
       <div className="input-group mb-3">
         <input
+          value={name}
+          onChange={(htmlElement) => setName(htmlElement.target.value)}
           placeholder="Nom du produit"
           type="text"
           className="form-control"
@@ -14,19 +67,34 @@ const AddProduct: FC<any> = () => {
           aria-describedby="inputGroup-sizing-default"
         />
       </div>
+      <label>Description du produit</label>
+
       <div className="input-group mb-3">
         <textarea
+          value={description}
+          onChange={(htmlElement) => setDescription(htmlElement.target.value)}
           placeholder="Description du produit"
           className="form-control"
           aria-label="With textarea"
         ></textarea>
       </div>
-      <select className="custom-select custom-select-lg mb-3">
-        <option selected>Open this select menu</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
+      <label>Category du produit</label>
+
+      <select
+        value={category}
+        onChange={(htmlElement) =>
+          setCategory(parseInt(htmlElement.target.value))
+        }
+        className="custom-select custom-select-lg mb-3"
+      >
+        {categories.map((category) => (
+          <option key={category.id} value={category.id}>
+            {category.name}
+          </option>
+        ))}
       </select>
+      <label>Image du produit</label>
+
       <IKContext
         publicKey="public_LV4KSYYDKUQ9OWZZM0ZIerfMH1s="
         urlEndpoint="https://ik.imagekit.io/cjvyejrxtm"
@@ -34,18 +102,26 @@ const AddProduct: FC<any> = () => {
         authenticationEndpoint="https://plantes-et-jardins-back.herokuapp.com/api/products/admin/uploadimage"
       >
         <div className="custom-file mb-3">
+          {/* TODO add image logic */}
           <IKUpload
             className="custom-file-input"
             id="image-input"
             fileName="my-upload"
+            onSuccess={(res)=>setImageUrl(res.data.path)}
           />
           <label className="custom-file-label" htmlFor="image-input">
             Choose image
           </label>
         </div>
       </IKContext>
+      <label>Prix de vente du produit</label>
+
       <div className="input-group mb-3">
         <input
+          value={sellingPrice}
+          onChange={(htmlElement) =>
+            setSellingPrice(parseFloat(htmlElement.target.value))
+          }
           placeholder="Prix de vente"
           type="number"
           className="form-control"
@@ -53,8 +129,14 @@ const AddProduct: FC<any> = () => {
           aria-describedby="inputGroup-sizing-default"
         />
       </div>
+      <label>Prix d'achat du produit</label>
+
       <div className="input-group mb-3">
         <input
+          value={buyingPrice}
+          onChange={(htmlElement) =>
+            setBuyingPrice(parseFloat(htmlElement.target.value))
+          }
           placeholder="Prix d'achat"
           type="number"
           className="form-control"
@@ -62,8 +144,14 @@ const AddProduct: FC<any> = () => {
           aria-describedby="inputGroup-sizing-default"
         />
       </div>
+      <label>Quantité du produit en stock</label>
+
       <div className="input-group mb-3">
         <input
+          value={quantity}
+          onChange={(htmlElement) =>
+            setQuantity(parseInt(htmlElement.target.value))
+          }
           placeholder="Nombre d'articeles en stock"
           type="number"
           className="form-control"
@@ -71,8 +159,14 @@ const AddProduct: FC<any> = () => {
           aria-describedby="inputGroup-sizing-default"
         />
       </div>
+      <label>Remise pour ce produit</label>
+
       <div className="input-group mb-3">
         <input
+          value={discount}
+          onChange={(htmlElement) =>
+            setDiscount(parseFloat(htmlElement.target.value))
+          }
           placeholder="Remise"
           type="number"
           className="form-control"
@@ -80,7 +174,24 @@ const AddProduct: FC<any> = () => {
           aria-describedby="inputGroup-sizing-default"
         />
       </div>
-      <button type="button" className="btn btn-success">Ajouter le produit</button>
+      <button
+        type="button"
+        onClick={() =>
+          postProduct(
+            name,
+            description,
+            category,
+            imageUrl,
+            sellingPrice,
+            buyingPrice,
+            quantity,
+            discount
+          )
+        }
+        className="btn btn-success"
+      >
+        Ajouter le produit
+      </button>
     </div>
   );
 };
